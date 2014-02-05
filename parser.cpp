4f7e9020624fe55be400e4988a0538c86878edc7
zachmckie <zach.mckie@gmail.com>
2014-02-04 23:07:20 -0800

    Created new file hierarchy
    db_engine.h and relation.h declarations created. Other files remain
    empty.

---
+++ parser.cpp
@@ -0,0 +1,7 @@
+/*File description: parser.cpp defines all member functions
+declared parser.h. The database engine is declared here.*/
+
+#include "parser.h"
+
+DB_Engine dbms; //Declare database engine referred to in parser
+
