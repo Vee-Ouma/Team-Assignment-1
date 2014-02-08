92d4327efef07ba2bc458fbfd5355ade2cfc1030
jordankarisch <jordankarisch@msn.com>
2014-02-08 00:18:32 -0600

    Implemented rename, projection, and print functions

---
+++ Pokedex/Pokedex/Pokedex/parser.h
@@ -0,0 +1,8 @@
+/*File description: paser.h declares the Parser class. The purpose
+of this class is to implement functions to parse each possible
+SQL command/query. Calls will be made to the */
+
+#pragma once
+
+#include "db_engine.h"
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+/*File description: main.cpp is the future home of the
+application. This file will likely be updated during phase II of
+development.*/
+
+int main () 
+{
+
+}
