b75f0f9ead53b0722d6ed230bb151034f288c117
jordankarisch <jordankarisch@msn.com>
2014-02-08 00:19:55 -0600

    Revert "Implemented rename, projection, and print functions"
    This reverts commit 92d4327efef07ba2bc458fbfd5355ade2cfc1030.

--- README.md
+++
@@ -1,4 +0,0 @@
-CSCE315_Database_Project
-========================
-
-Database Assignment
--- db_engine.cpp
+++
@@ -1,4 +0,0 @@
-/*File description: db_engine.cpp defines all member functions
-declared in db_engine.h*/
-
-#include "db_engine.h"
