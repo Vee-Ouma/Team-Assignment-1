5496e5ec7a98d38ccb135ab7f743d6a6268f267d
jordankarisch <jordankarisch@msn.com>
2014-02-08 00:21:43 -0600

    Implemented rename, projection, and print functions

---
+++ Pokedex/Pokedex/Pokedex/db_engine.cpp
@@ -0,0 +1,4 @@
+/*File description: db_engine.cpp defines all member functions
+declared in db_engine.h*/
+
+#include "db_engine.h"
---
+++ Pokedex/Pokedex/Pokedex/relation.h
@@ -0,0 +1,108 @@
+/*File description: relation.h declares the Relation class. Objects
+of this class are tables resembled by a 2-D vector of strings. Each
+row of the table signifies a tuple and each column signifies an
+attribute. The member functions defined in this header must support
+updating and accessing data in the tables.*/
+
+/*What to to think about: Need to declare and define a function that
+retuns a boolean value whether an attribute's value satisfies a condition.
+Any functions that are passed vector<string> cond_list will need to call
+this function.*/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Relation {
+	string name;          //Name of relation
+	vector<int> key_pos; //Contains position of columns used as keys
+	
+	vector<string> attr_names;	//Name of the attributes
+	vector<string> attr_types; 	//Data type of the attributes
+	
+	vector<vector<string>> table; //2-Dimensional table of strings
+	
+	Relation() //Default relation constructor
+	{
+		name = "";
+	}
+
+	Relation(string _name, vector<string> _attr, vector<string> _attr_type, vector<int> _key_pos)
+	{
+		name = _name;
+		key_pos = _key_pos;
+		table.push_back(_attr);
+		table.push_back(_attr_type);
+	}
+
+	/*------------------ Helper Functions ------------------*/
+
+	//Return name of Relation
+	string get_name()
+	{
+		return name;
+	}
+
+	//Set name of Relation
+	void set_name(string new_name)
+	{
+		name = new_name;
+	}
+	
+	//Return true if attribute's value satisfies condition
+	bool test_condition(vector<string> attr_list, vector<string> cond_list);
+
+	/*------------------- Row Operations -------------------*/
+	
+	//Insert values into a row of a table TODO
+	void insert_row(vector<string> value_list);
+	
+	//Delete rows where attribute satisfies condition TODO
+	void delete_row(vector<string> attr_list, vector<string> cond_list);
+	
+	/*----------------- Column Operations ------------------*/
+
+    //Insert values into a column of a table TODO
+    void insert_column(vector<string> value_list);
+		
+	//Select values in columns that satisfy conditions TODO
+	vector<string> select_column(vector<string> attr_list, vector<string> cond_list);
+	
+	//Update values in columns that satisfy conditions TODO
+	void update_column(vector<string> attr_list, vector<string> cond_list);
+	
+	/*----------------- Relation Operations ----------------*/
+	
+	//Returns true if two relations are union compatible
+	bool union_compatible(Relation table); //Alex
+
+	//Return new relation with renamed attributes
+	void renaming(vector<string> attr_list); //Jordan
+
+	//Return new relation that contains speficied attribute columns
+	Relation projection(vector<string> attr_list); //Jordan
+
+	//Return new relation that satisfies conditions
+	Relation selection(vector<string> attr_list, vector<string> cond_list); //Jordan
+
+	//Return new relation which is the union of two relations
+	Relation set_union(Relation table); //Zach
+
+	//Return new relation which is the difference of two relations
+	Relation set_difference(Relation table); //Zach
+
+	//Return new relation which is the cross product of two relations
+	Relation cross_product(Relation table); //Ian
+
+	//Return new relation which is the natural join of two relations
+	Relation natural_join(Relation table); //Ian
+
+	/*------------------ Print Operations ------------------*/
+
+	//Display relation to console 
+	void show(); //Jordan
+	
+};
