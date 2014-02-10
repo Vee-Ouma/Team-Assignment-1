ed0a047043906ecd504de899cc82e6040b0e3076
skinburn24 <skinburn24@gmail.com>
2014-02-10 17:46:08 -0600

    Alex test

--- Pokedex/Pokedex/Pokedex/relation.cpp
+++ Pokedex/Pokedex/Pokedex/relation.cpp
@@ -4,5 +4,18 @@ declared in relation.h*/
 #include "relation.h"
 
 
+/*------------------- Row Operations -------------------*/
+	
+	//Insert values into a row of a table
+	void insert_row(vector<string> value_list)
+	{
 
+	}
+	
+	//Delete rows where attribute satisfies condition
+	void delete_row(vector<string> attr_list, vector<string> cond_list)
+	{
+
+	}
+/*-------------------End Row Operations ----------------*/	
  
